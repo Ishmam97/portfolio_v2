0b6a2d4c167f475ca02dae95c1d57b75b58feb0a
Ishmam <ishmam.a.solaiman@gmail.com>
2025-01-12 04:26:27 -0600

    add about me timeline in homepage

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React from 'react';
+import './css/AboutMe.css';
+
+import { Container, Typography, List, ListItem, ListItemText} from '@mui/material';
+import { Timeline, Events, Event } from 'vertical-timeline-component-react';
+
+import { resumeExperience } from './experienceData';
 
 export default function AboutMe() {
+  const customTheme = {
+    borderDotColor: '#372963',
+    descriptionColor: '#c592ff',
+    dotColor: '#c592ff',
+    eventColor: '#DFFF3D',
+    lineColor: '#00FF9C',
+    subtitleColor: '#ff4081',
+    titleColor: '#00FF9C',
+    yearColor: '#ff4081',
+  };
+
   return (
-    <div>AboutMe</div>
-  )
+    <Container className='section-container' sx={{ color: 'white', py: 4 }}>
+      <Typography variant="h3" sx={{justifySelf: 'center', color: '#DFFF3D'}}>
+        My Journey
+      </Typography>
+
+      <Timeline
+        lang="en"
+        theme={customTheme}
+        dateFormat="only-number"
+        collapse
+        withoutDay
+      >
+        {resumeExperience.map((item, index) => {
+        
+          const bulletList = (
+            <List sx={{ listStyleType: 'disc', pl: 4 }}>
+              {item.bullets.map((bullet, i) => (
+                <ListItem key={i} sx={{ display: 'list-item', py: 0 }}>
+                  <ListItemText primary={bullet} />
+                </ListItem>
+              ))}
+            </List>
+          );
+
+          const descriptionArray = [bulletList];
+
+          return (
+            <Events 
+            key={index} 
+            title={item.title} 
+            subtitle={item.company}
+            startDate={item.startDate}
+            endDate={item.endDate} 
+>
+              <Event title="Details" description={descriptionArray}/>
+            </Events>
+          );
+        })}
+      </Timeline>
+    </Container>
+  );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import AboutMe from '../components/AboutMe'
+import './css/HomePage.css'
 
 const HomePage = () => {
   return (
     <>
         <Hero />
+        <AboutMe />
     </>
   )
 }
